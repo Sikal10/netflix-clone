2ae18d219fc17d3292670cd9a5b45056ff573360
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-02-18 09:34:00 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtkMf7iR-rjkTHlGD8V-dKY--A3_ZUNWo",
+    authDomain: "netflix-clone-d1d81.firebaseapp.com",
+    projectId: "netflix-clone-d1d81",
+    storageBucket: "netflix-clone-d1d81.appspot.com",
+    messagingSenderId: "1003912079458",
+    appId: "1:1003912079458:web:68a79adb4eee6c136b093b"
+};
